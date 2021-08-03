bfe5563816a8622d3202f7b30b63b879efba257f
Floony7 <floony7@yahoo.com>
2021-08-03 15:46:26 +1200

    Added loading state

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ export default function App() {
 
   const [hits, setHits] = useState([])
   const [query, setQuery] = useState('reacthooks')
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
    fetchData()
@@ -15,9 +16,11 @@ export default function App() {
   }, [])
 
   const fetchData = async () => {
+    setLoading(true)
     const res = await axios.get(`http://hn.algolia.com/api/v1/search?query=${query}`)
     setHits(res.data.hits)
-
+    console.log(hits)
+    setLoading(false)
   }
 
 const handleSubmit = (e) => {
@@ -31,15 +34,21 @@ const handleSubmit = (e) => {
        <input type="text" name="search" value={query} onChange={e => setQuery(e.target.value)} />
        <input type="submit" value="Submit" />
      </form>
-   <ul className="articles">
-    {
-     hits.map(hit => (
-       <li className="item" key={hit.objectID}>
-         <a href={hit.url} target="_blank" rel="noreferrer">{hit.title}</a>
-       </li>
-     )) 
-    }
-    </ul>
+  {
+    loading ? ( 
+    <div className="m-3">Loading stories...</div>
+    ) : (
+      <ul className="articles">
+      {
+       hits.map(hit => (
+         <li className="item" key={hit.objectID}>
+           <a href={hit.url} target="_blank" rel="noreferrer">{hit.title}</a>
+         </li>
+       )) 
+      }
+      </ul>
+    )
+  }
    </div>
   );
 }
